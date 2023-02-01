a101a18c68ffc791efa34929fd57cb31f0e8ee7b
AsyrafbilalFBJ <asyrafbilalrpltelu@student.telkomuniversity.ac.id>
2023-02-01 17:39:44 +0700

    Done all

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="row bg-primary" style="height:1.5vh">&ensp;</div>
+    <div class="row m-5">
+        <div class="col-1">
+        </div>
+        <div class="col-lg-7 col-sm-12">
+            <router-link to="/" exact class="text-decoration-none">
+                <h5 class="text-uppercase text-dark fw-semibold m-0 align-middle">Asyrafbilal Fadhila Bhinar Jaya</h5>
+            </router-link>
+        </div>
+        <div class="col-1">
+        </div>
+        <div class="col-lg-1 col-sm-12">
+            <router-link to="/" exact class="text-decoration-none text-end txt-black">
+                <p class="text-uppercase fw-normal m-0">Work</p>
+            </router-link>
+        </div>
+        <div class="col-lg-1 col-sm-12">
+            <router-link to="/about" exact class="text-decoration-none text-end txt-black">
+                <p class="text-uppercase fw-normal m-0">About</p>
+            </router-link>
+        </div>
+        <div class="col-1">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavHeader',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+.txt-black{
+    color: black
+}
+.router-link-exact-active{
+    color: #0D6EFD
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../components/HomePage.vue'
+import About from '../components/AboutPage.vue'
+import ProductDisplay from '../components/FirstStuff.vue'
+import Unit3C from '../components/SecondStuff.vue'
+import GadgetCatalog from '../components/ThirdStuff.vue'
+
+const routes = [
+    { 
+        path: '/', 
+        component: Home 
+    },
+    { 
+        path: '/about', 
+        component: About 
+    },
+    { 
+        path: '/ProductDisplay', 
+        component: ProductDisplay 
+    },
+    { 
+        path: '/Unit3C', 
+        component: Unit3C 
+    },
+    { 
+        path: '/GadgetCatalog', 
+        component: GadgetCatalog 
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
