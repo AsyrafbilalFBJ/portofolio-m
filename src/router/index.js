a870c8652d259d53109ca70c772f37d3d1c1a2f2
asyrafbg <c360dsx2422@bangkit.academy>
2025-07-09 23:50:50 +0700

    add path createWebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory('/portofolio-m/'),
     routes,
     scrollBehavior() {
         // always scroll to top
